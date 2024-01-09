90a641095081a547c51bd025c686576d26fd694b
LFloresMgsa <107148012+LFloresMgsa@users.noreply.github.com>
2024-01-09 12:03:43 -0500

    1

--- src/views/Soporte.js
+++ src/views/Soporte.js
@@ -1,13 +1,21 @@
-// En Soporte.js
 import React from 'react';
-import fondo from '../imagenes/soporte.png'
+import { Container, CssBaseline, Box, Typography } from '@mui/material';
+import fondo from '../imagenes/soporte.png';
+
 const Soporte = (props) => {
   // ... lógica del componente ...
 
   return (
-    <div className="container">
-       <img src="../imagenes/soporte.png" alt="" />
-    </div>
+    <Container component="main" maxWidth="xs" sx={{ height: '100vh', display: 'flex', alignItems: 'center' }}>
+      <CssBaseline />
+      <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <img src={fondo} alt="" style={{ width: '150%', opacity: 0.7 }} />
+        {/* Agrega cualquier otra lógica o contenido aquí */}
+        <Typography component="div" variant="body1">
+          {/* Contenido adicional si es necesario */}
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
